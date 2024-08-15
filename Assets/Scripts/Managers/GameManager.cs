3a9d6272bab612fd2476beb282ac06179ba76d56
Slidoboss <usoloudo@gmail.com>
2024-08-15 13:36:37 +0100

    Added a function to add the different managers to the game manager in the scene.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance { get; private set;}
+    public static GameManager Instance { get; private set; }
+    public static DeckManager DeckManager{ get; private set; }
+    public static AudioManager AudioManager { get; private set; }
+    public static OptionsManager OptionsManager { get; private set; }
     private int _playerHealth;
     private int _playerXP;
     private int _difficulty = 5;
@@ -15,15 +19,79 @@ public class GameManager : MonoBehaviour
         //Cons of this design pattern are, data that the instance holds only persists during the games session.
         //if you need to save data between sessions you'll need another form of persistent storage.
         //Tight coupling is also another con.
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            InitializeManagers();
         }
-        else if(Instance != this)
+        else if (Instance != this)
         {
             Destroy(gameObject);
         }
+    }
+
+    private void InitializeManagers()
+    {
+        DeckManager = GetComponentInChildren<DeckManager>();
+        AudioManager = GetComponentInChildren<AudioManager>();
+        OptionsManager = GetComponentInChildren<OptionsManager>();
+
+        if(DeckManager == null)
+        {
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/DeckManager"); 
+            if(prefab == null)
+            {
+                Debug.Log("DeckManager prefab not found");
+            }
+            else
+            {
+                Instantiate(prefab,transform.position,Quaternion.identity,transform);
+                DeckManager = GetComponentInChildren<DeckManager>();
+            }
+        }
+        if(AudioManager == null)
+        {
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/AudioManager"); 
+            if(prefab == null)
+            {
+                Debug.Log("AudioManager prefab not found");
+            }
+            else
+            {
+                Instantiate(prefab,transform.position,Quaternion.identity,transform);
+                AudioManager = GetComponentInChildren<AudioManager>();
+            }
+        }
+        if(OptionsManager == null)
+        {
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/OptionsManager"); 
+            if(prefab == null)
+            {
+                Debug.Log("OptionsManager prefab not found");
+            }
+            else
+            {
+                Instantiate(prefab,transform.position,Quaternion.identity,transform);
+                OptionsManager = GetComponentInChildren<OptionsManager>();
+            }
+        }
+    }
+
+    public int PlayerHealth
+    {
+        get { return _playerHealth; }
+        set { _playerHealth = value; }
+    }
+    public int PlayerXP
+    {
+        get { return _playerXP; }
+        set { _playerXP = value; }
+    }
+    public int Difficulty
+    {
+        get { return _difficulty; }
+        set { _difficulty = value; }
     }
 
 }
