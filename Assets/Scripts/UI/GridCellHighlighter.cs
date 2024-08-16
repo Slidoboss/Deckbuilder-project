1c3929209a13c015868562b3a5704520182e7857
Slidoboss <usoloudo@gmail.com>
2024-08-16 12:52:01 +0100

    Added script to highlight cell in grid when mouse hovers over it.

---
+++ Assets/Scripts/UI/GridCellHighlighter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class GridCellHighlighter : MonoBehaviour
+{
+    private SpriteRenderer _cellSprite;
+    private Color _originalColor;
+    private Color _highlightColor = Color.cyan;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cellSprite = GetComponent<SpriteRenderer>();
+        _originalColor = _cellSprite.color;
+    }
+
+    void OnMouseEnter()
+    {
+        _cellSprite.color = _highlightColor;
+    }
+    void OnMouseExit()
+    {
+        _cellSprite.color = _originalColor;
+    }
+}
