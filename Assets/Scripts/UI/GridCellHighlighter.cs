acba62b23bf01dc388413d9fdb067e8e8b4ef094
Slidoboss <usoloudo@gmail.com>
2024-08-28 12:42:35 +0100

    Added selective highlighting.

--- Assets/Scripts/UI/GridCellHighlighter.cs
+++ Assets/Scripts/UI/GridCellHighlighter.cs
@@ -7,17 +7,33 @@ public class GridCellHighlighter : MonoBehaviour
 {
     private SpriteRenderer _cellSprite;
     private Color _originalColor;
-    private Color _highlightColor = Color.red;
+    private Color _highlightColor = Color.cyan;
+    private Color _positiveColor = Color.green;
+    private Color _negativeColor = Color.red;
+    private GridCell _gridCell;
+
     // Start is called before the first frame update
     void Start()
     {
         _cellSprite = GetComponent<SpriteRenderer>();
+        _gridCell = GetComponent<GridCell>();
         _originalColor = _cellSprite.color;
     }
 
     void OnMouseEnter()
     {
-        _cellSprite.color = _highlightColor;
+        if (!GameManager.Instance.isPlayingCard)
+        {
+            _cellSprite.color = _highlightColor;
+        }
+        else if (_gridCell.cellFull || _gridCell.gridIndex.x > 1)
+        {
+            _cellSprite.color = _negativeColor;
+        }
+        else
+        {
+            _cellSprite.color = _positiveColor;
+        }
     }
     void OnMouseExit()
     {
